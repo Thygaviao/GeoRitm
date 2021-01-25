3cb34752c9baea128141f8c3e4d8b82df0d6bcb8
Kirill <thygaviao@yandex.ru>
2021-01-25 13:19:39 +0300

    Обернул авторизацию в функцию

--- authorization.py
+++ authorization.py
@@ -2,17 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-print('Версия теста 1.0. Когда-нибудь и на PageObject перейдём...')
+def test_autorization():
+    print('Версия теста 1.0. Когда-нибудь и на PageObject перейдём...')
 
-link = str(input('Введите адрес инсталляции GeoRITM. Не забудьте про порт: '))
-browser = webdriver.Chrome()
-browser.implicitly_wait(15)
-browser.get(link)
+    link = str(input('Введите адрес инсталляции GeoRITM. Не забудьте про порт: '))
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(15)
+    browser.get(link)
 
-browser.find_element(By.NAME, 'login').send_keys('root')
-browser.find_element(By.NAME, 'password').send_keys('password')
-browser.find_element(By.XPATH, '//*[@id="auth"]/div/div/div[1]/form/div[3]/div/button').click()
+    browser.find_element(By.NAME, 'login').send_keys('root')
+    browser.find_element(By.NAME, 'password').send_keys('password')
+    browser.find_element(By.XPATH, '//*[@id="auth"]/div/div/div[1]/form/div[3]/div/button').click()
 
-time.sleep(10)
-browser.quit()
+    time.sleep(10)
+    browser.quit()
 
