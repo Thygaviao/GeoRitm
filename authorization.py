ed58c6a7681c7cc116e239de16669648e204cbdd
Kirill <thygaviao@yandex.ru>
2021-01-26 10:42:15 +0300

    Исправлена опечатка в функции

--- authorization.py
+++ authorization.py
@@ -2,17 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-def test_autorization():
+def test_authorization():
     print('Версия теста 1.0. Когда-нибудь и на PageObject перейдём...')
 
-    link = str(input('Введите адрес инсталляции GeoRITM. Не забудьте про порт: '))
+    link = "http://10.78.15.5:8080/"
     browser = webdriver.Chrome()
     browser.implicitly_wait(15)
     browser.get(link)
 
     browser.find_element(By.NAME, 'login').send_keys('root')
     browser.find_element(By.NAME, 'password').send_keys('password')
-    browser.find_element(By.XPATH, '//*[@id="auth"]/div/div/div[1]/form/div[3]/div/button').click()
+    browser.find_element(By.XPATH, '//*[@id="auth"]/div/div/div[1]/form/div[3]/button').click()
 
     time.sleep(10)
     browser.quit()
